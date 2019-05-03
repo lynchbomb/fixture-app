737532caf799c431a51122078f821de3298a372e
Marc Lynch <lynchmarc2@gmail.com>
2019-05-03 10:19:51 -0700

    travis tb compare

--- config/environment.js
+++ config/environment.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports = function(environment) {
+module.exports = function (environment) {
   let ENV = {
     modulePrefix: 'fixture-app',
     environment,
@@ -45,6 +45,8 @@ module.exports = function(environment) {
 
   if (environment === 'production') {
     // here you can enable a production-specific feature
+    ENV.locationType = 'hash';
+    ENV.rootURL = '';
   }
 
   return ENV;
